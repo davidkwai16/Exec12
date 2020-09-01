2e67af884ffb824d03a87d3efbe671950d1cebc3
davidkwai16 <dlkp16@gmail.com>
2020-09-01 14:07:24 -0300

    Primeiro

---
+++ Exec12/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿using Exec12.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exec12.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus orderStatus, Client client)
+        {
+            Moment = moment;
+            OrderStatus = orderStatus;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem items)
+        {
+            Items.Add(items);
+        }
+
+        public void RemoveItem(OrderItem items)
+        {
+            Items.Remove(items);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY:");
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + OrderStatus);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem c in Items)
+            {
+                sb.AppendLine(c.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2",CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exec12/Program.cs
@@ -0,0 +1,49 @@
+﻿using Exec12.Entities;
+using Exec12.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace Exec12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Status: ");
+            
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Client client = new Client(name, email, birthdate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter #" + i + " item data:");
+                Console.Write("Product name: ");
+                string pname = Console.ReadLine();
+                Console.Write("Product price: ");
+                double pprice = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                Product product = new Product(pname, pprice);
+
+                Console.Write("Quantity: ");
+                int pquantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderitem = new OrderItem(pquantity, pprice, product);
+
+                order.AddItem(orderitem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(order);
+        }
+    }
+}
